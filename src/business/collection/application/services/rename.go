4f7ecd44cbe232795f52130627bd8470b140cb2c
Silas Enrique <silas.enriques@gmail.com>
2024-05-20 07:46:52 -0300

    adicionando o teste para renomear uma coleção

--- src/business/collection/application/services/rename.go
+++ src/business/collection/application/services/rename.go
@@ -1,23 +1,29 @@
 package service
 
 import (
+	"database/sql"
 	"errors"
 	"go-read-list/src/business/collection/application/command"
 	"go-read-list/src/business/collection/application/mapper"
+	"strings"
 	"time"
 )
 
 func (f *CollectionService) Rename(cmd *command.CollectionRenameCommand) (*command.CollectionResponse, error) {
 	if cmd.Id == 0 {
-		return nil, errors.New("o id deve ser informado")
+		return nil, ErrIdEqualZero
 	}
 
-	if cmd.Name == "" {
-		return nil, errors.New("o nome deve ser informado")
+	if strings.Trim(cmd.Name, " ") == "" {
+		return nil, ErrNameIsNull
 	}
 
 	colle, err := f.rep.GetById(cmd.Id)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotExist
+		}
+
 		return nil, err
 	}
 
