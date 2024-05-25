a0eeba955538cdc00191d63115ea198d147d5ed4
Silas Enrique <silas.enriques@gmail.com>
2024-05-25 15:46:09 -0300

    melhorando a interface da remoção de uma collection pelo seu id unico

--- src/business/interface/cli/delete.go
+++ src/business/interface/cli/delete.go
@@ -13,18 +13,11 @@ func deleteCmds(wrapperRepository *wrapper) []*cli.Command {
 			Name:    "collection",
 			Aliases: []string{"c"},
 			Usage:   "deleta uma collection",
-			Subcommands: []*cli.Command{
-				{
-					Name:    "byId",
-					Aliases: []string{"id"},
-					Usage:   "excluir uma coleção utilizando o id",
-					Flags: []cli.Flag{
-						&cli.Int64Flag{Name: "id", Aliases: []string{"i"}, Usage: "id da pasta", Required: true},
-						&cli.BoolFlag{Name: "recursive", Aliases: []string{"r"}, Usage: "indica se pode excluir quem depende dessa collection", Value: false},
-					},
-					Action: colli.RemoveById(wrapperRepository.collection),
-				},
+			Flags: []cli.Flag{
+				&cli.Int64Flag{Name: "id", Aliases: []string{"i"}, Usage: "id da pasta", Required: true},
+				&cli.BoolFlag{Name: "recursive", Aliases: []string{"r"}, Usage: "indica se pode excluir quem depende dessa collection", Value: false},
 			},
+			Action: colli.RemoveById(wrapperRepository.collection),
 		},
 		{
 			Name:    "bookmark",
